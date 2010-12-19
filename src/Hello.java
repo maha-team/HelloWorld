19caac8a13488357135076726a70afbd7f8ca8a3
André <carlsson_andre@192.168.1.3>
2010-12-20 00:11:12 +0100

    OKAY
    Signed-off-by: André <carlsson_andre@192.168.1.3>

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
 
 public class Hello {
 	
+	/**
+	 * IMPOSSIBLE TO UNDERSTAND THIS GITSHITGITSHIT!!!!
+	 * @param arghs
+	 */
+	
 	public static void main(String[] arghs) {
 		System.out.println("HELLO WORLD");
 		
